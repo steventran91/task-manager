19a3a8424546ed0bda8f0503285a3546f6b4aa6f
Steven Tran <sdtran91@gmail.com>
2026-05-28 17:13:22 -0700

    create all 5 api endpoints

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,46 @@
 class TasksController < ApplicationController
+    def index
+        tasks = Task.all 
+        render json: tasks 
+    end 
 
+    def create 
+        task = Task.new(task_params)
+        if task.save
+            render json: task, status: :created
+        else
+            render json: task.errors, status: :unprocessable_entity
+        end 
+    end
+
+    def show
+        task = Task.find(params[:id])
+        render json: task 
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: 'Task not found'}, status: :not_found
+    end
+
+    def update
+        current_task = Task.find(params[:id])
+        if current_task.update(task_params)
+            render json: current_task, status: :ok
+        else
+            render json: current_task.errors, status: :unprocessable_entity
+        end
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: 'Task not found'}, status: :not_found
+    end 
+
+    def destroy
+        task = Task.find(params[:id])
+        task.destroy
+        head :no_content
+    rescue ActiveRecord::RecordNotFound
+        render json: {error: 'Task not found'}, status: :not_found
+    end
+
+    private 
+    def task_params
+        params.require(:task).permit(:title, :description, :status, :due_date)
+    end
 end
