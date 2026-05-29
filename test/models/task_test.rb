57e5409e5f8046414ac652a61eb10e406b6cabb0
Steven Tran <sdtran91@gmail.com>
2026-05-28 17:26:32 -0700

    create validation minitests for tasks

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid task saves successfully" do
+    task = Task.new(title: "Make coffee", status: "pending")
+    assert task.valid?
+  end
+
+  test "invalid task without title" do
+    task = Task.new(status: "pending")
+    assert_not task.valid?
+  end
+
+  test "invalid task without status" do
+    task = Task.new(title: "Buy groceries")
+    assert_not task.valid? 
+  end
+
+  test "invalid task with invalid status" do
+    task = Task.new(title: "Do laundry", status: "tomorrow")
+    assert_not task.valid?
+  end
 end
